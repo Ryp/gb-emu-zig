ea760a6468bbc6bf3f02cdaa4c955887f2b9b35d
Ryp <thibault.schueller@protonmail.com>
2024-05-09 20:06:30 +0300

    emu: increase ROM parsing memory
    Add registers debug

--- src/main.zig
+++ src/main.zig
@@ -31,14 +31,26 @@ pub fn main() !void {
     defer file.close();
 
     // FIXME I don't support ROMs with swappable address space
-    var buffer: [1024 * 4]u8 = undefined; // FIXME
+    var buffer: [256 * 128]u8 = undefined; // FIXME
     const rom_bytes = try file.read(buffer[0..buffer.len]);
 
     var gb_state = try gb.create_state(gpa_allocator, buffer[0..rom_bytes]);
     defer gb.destroy_state(gpa_allocator, &gb_state);
 
     while (true) {
-        std.debug.print("loop: pc = {}\n", .{gb_state.registers.pc});
+        std.debug.print("===================================\n", .{});
+        std.debug.print("====| PC = {x:4}, SP = {x:4}\n", .{ gb_state.registers.pc, gb_state.registers.sp });
+        std.debug.print("====| A = {x:2}, Flags: {s} {s} {s} {s}\n", .{
+            gb_state.registers.a,
+            if (gb_state.registers.flags.zero == 1) "Z" else "_",
+            if (gb_state.registers.flags.substract == 1) "N" else "_",
+            if (gb_state.registers.flags.half_carry == 1) "H" else "_",
+            if (gb_state.registers.flags.carry == 1) "C" else "_",
+        });
+        std.debug.print("====| B = {x:2}, C = {x:2}\n", .{ gb_state.registers.b, gb_state.registers.c });
+        std.debug.print("====| D = {x:2}, E = {x:2}\n", .{ gb_state.registers.d, gb_state.registers.e });
+        std.debug.print("====| H = {x:2}, L = {x:2}\n", .{ gb_state.registers.h, gb_state.registers.l });
+        std.debug.print("===================================\n", .{});
 
         const i = try instructions.decode(gb_state.memory[gb_state.registers.pc..]);
 
