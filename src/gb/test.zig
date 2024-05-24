f630cf4c1d7f63fabb3a8f7f0da27023da81121b
Ryp <thibault.schueller@protonmail.com>
2024-05-24 18:48:25 +0300

    test: add more things and print all possible instructions

--- src/gb/test.zig
+++ src/gb/test.zig
@@ -14,23 +14,71 @@ test {
 
     const rom_bytes = [2]u8{ 3, 3 };
 
-    var gb_state = try cpu.create_state(allocator, &rom_bytes);
-    defer cpu.destroy_state(allocator, &gb_state);
+    var gb = try cpu.create_state(allocator, &rom_bytes);
+    defer cpu.destroy_state(allocator, &gb);
 
-    try expectEqual(0x00, gb_state.registers.b);
-    try expectEqual(0x13, gb_state.registers.c);
-    try expectEqual(0x00, gb_state.registers.d);
-    try expectEqual(0xD8, gb_state.registers.e);
-    try expectEqual(0x01, gb_state.registers.h);
-    try expectEqual(0x4D, gb_state.registers.l);
-    try expectEqual(0x01, gb_state.registers.a);
+    try expectEqual(0x00, gb.registers.b);
+    try expectEqual(0x13, gb.registers.c);
+    try expectEqual(0x00, gb.registers.d);
+    try expectEqual(0xD8, gb.registers.e);
+    try expectEqual(0x01, gb.registers.h);
+    try expectEqual(0x4D, gb.registers.l);
+    try expectEqual(0x01, gb.registers.a);
     try expectEqual(cpu.FlagRegister{
         ._unused = 0,
         .carry = 1,
         .half_carry = 1,
         .substract = false,
         .zero = true,
-    }, gb_state.registers.flags);
-    try expectEqual(0xFFFE, gb_state.registers.sp);
-    try expectEqual(0x0100, gb_state.registers.pc);
+    }, gb.registers.flags);
+    try expectEqual(0xFFFE, gb.registers.sp);
+    try expectEqual(0x0100, gb.registers.pc);
+
+    gb.registers.a = 25;
+    execution.execute_instruction(&gb, .{ .byte_len = 1, .encoding = .{ .add_a_r8 = .{ .r8 = instructions.R8.a } } });
+
+    try expectEqual(50, gb.registers.a);
+
+    gb.registers.b = 1;
+    execution.execute_instruction(&gb, .{ .byte_len = 1, .encoding = .{ .add_a_r8 = .{ .r8 = instructions.R8.b } } });
+
+    try expectEqual(1, gb.registers.b);
+
+    execution.execute_instruction(&gb, .{ .byte_len = 1, .encoding = .{ .res_b3_r8 = .{ .bit_index = 0, .r8 = instructions.R8.b } } });
+
+    try expectEqual(0, gb.registers.b);
+
+    try expectEqual(51, gb.registers.a);
+}
+
+test {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    const rom_bytes = [2]u8{ 3, 3 };
+
+    var gb = try cpu.create_state(allocator, &rom_bytes);
+    defer cpu.destroy_state(allocator, &gb);
+
+    for (0..256) |op_code| {
+        const instruction_bytes = [3]u8{ @intCast(op_code), 0xff, 0xff };
+
+        const instruction = try instructions.decode(&instruction_bytes);
+
+        std.debug.print("{x:0>2} {b:0>8} ", .{ op_code, op_code });
+
+        instructions.debug_print(instruction);
+    }
+
+    for (0..256) |op_code| {
+        const instruction_bytes = [3]u8{ 0xcb, @intCast(op_code), 0xff };
+
+        const instruction = try instructions.decode(&instruction_bytes);
+
+        std.debug.print("{x:0>2} {b:0>8} ", .{ op_code, op_code });
+
+        instructions.debug_print(instruction);
+    }
 }
